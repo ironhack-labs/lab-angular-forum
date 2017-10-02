05a3701be220ca634b5453f8b623308acf6d0b1d
Mercedes Egido <meegido@gmail.com>
2017-10-02 21:54:27 +0200

    prepared auth login and signup

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -8,18 +8,21 @@ import {routes} from './routes';
 import { AppComponent } from './app.component';
 import { ThreadsService } from './services/threads.service'
 import { ThreadsListComponent } from './threads-list/threads-list.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './services/auth.service'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ThreadsListComponent
+    ThreadsListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [ThreadsService],
+  providers: [ThreadsService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
