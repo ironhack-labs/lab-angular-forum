72061b32f83cea73982a294ce3fd501a6f84ffbc
Sara <saraeme20@gmail.com>
2017-12-03 19:51:10 +0100

    first commit

---
+++ starter-code/client/src/app/models/usermodel.ts
@@ -0,0 +1,4 @@
+export class Thread {
+  title: string;
+  date: string;
+}
---
+++ starter-code/client/src/app/routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { AllComponentComponent } from './all-component/all-component.component';
+
+export const routes: Routes = [
+    { path: '', component: AllComponentComponent },
+    // { path: 'entry/:id', component: SingleEntryComponent },
+
+];
