fbdda9dcd0a5f7cb957c6a7857ba79135232672a
Sergio <sergiovelez.svg@gmail.com>
2018-02-26 09:17:25 +0100

    IT2

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { ThreadsServices } from '../services/threads.service';
 import { ListThreadComponent } from './list-thread/list-thread.component';
 import { HttpModule } from '@angular/http';
-
+import { HeaderComponent } from './header/header.component';
+import {AuthService} from '../services/auth.service';
+import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ListThreadComponent
+    ListThreadComponent,
+    HeaderComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpModule
   ],
-  providers: [ThreadsServices],
+  providers: [ThreadsServices, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
