7bef7f0fb7db75a5ef0bc2abef1e7c179a35854c
A-Tr <alvarotrancon@gmail.com>
2018-05-05 19:12:10 +0200

    login y logut funcionan

--- starter-code/client/src/app/app.component.ts
+++ starter-code/client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SessionService } from './session.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ElForUm';
+  constructor(public sessionService: SessionService) {
+    this.sessionService.userEvent.subscribe(user => {
+      console.log('USER EVENT');
+      if (user) {
+        this.title = `HOLA ${user.username}`;
+      } else {
+        this.title = 'PLIZ LOGIARSE!';
+      }
+    });
+  }
+  logout() {
+    this.sessionService.logout().subscribe();
+  }
 }
--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -2,16 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MyThreadsComponent } from './my-threads/my-threads.component';
 import { ThreadsService } from './threads.service';
 import { routes } from './routes';
+import { AuthLoginComponent } from './auth-login/auth-login.component';
+import { AuthSignupComponent } from './auth-signup/auth-signup.component';
+import { SessionService } from './session.service';
 
 @NgModule({
-  declarations: [AppComponent, MyThreadsComponent],
-  imports: [BrowserModule, HttpModule, RouterModule.forRoot(routes)],
-  providers: [ThreadsService],
+  declarations: [AppComponent, MyThreadsComponent, AuthLoginComponent, AuthSignupComponent],
+  imports: [BrowserModule, HttpModule, RouterModule.forRoot(routes), FormsModule],
+  providers: [ThreadsService, SessionService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
