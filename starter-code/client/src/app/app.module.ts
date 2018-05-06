e87643356bd5c0e8583d37c042232851768fb990
FernandoGalende <fernandogalende@gmail.com>
2018-05-06 18:12:08 +0200

    segunda iteracion terminada

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -3,17 +3,23 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router'
 import { FormsModule } from '@angular/forms';
+import { SessionService } from "./session.service";
+
 
 import { AppComponent } from './app.component';
 import { ListAllThreadsComponent } from './listAllThreads/listAllThreads.component';
+import { AuthSignupComponent } from './auth-signup/auth-signup.component';
 import { routes } from './routes';
+import { AuthLoginComponent } from './auth-login/auth-login.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListAllThreadsComponent
+    ListAllThreadsComponent,
+    AuthSignupComponent,
+    AuthLoginComponent
 ],
   imports: [
     BrowserModule,
@@ -21,7 +27,7 @@ import { routes } from './routes';
     HttpModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [SessionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- starter-code/client/src/app/routes.ts
+++ starter-code/client/src/app/routes.ts
@@ -1,8 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ListAllThreadsComponent } from './listAllThreads/listAllThreads.component';
+import { AuthSignupComponent } from './auth-signup/auth-signup.component';
+import { AuthLoginComponent } from './auth-login/auth-login.component';
+
 
 export const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: ListAllThreadsComponent },
+  { path: 'signup', component: AuthSignupComponent },
+  { path: 'login', component: AuthLoginComponent },
+
+
 ];
 
