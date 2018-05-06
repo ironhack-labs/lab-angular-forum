5c91ea44e4c03b06c38329ee1b1f8eeb5516e570
luisnueda <nueda.luis@gmail.com>
2018-05-06 22:33:39 +0200

    Iteration 4 and 5 done

--- starter-code/client/src/app/SingleThread/SingleThread.component.ts
+++ starter-code/client/src/app/SingleThread/SingleThread.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ThreadsService } from '../services/Threads.service';
+import { SessionService } from '../services/Session.service';
 
 @Component({
   selector: 'app-SingleThread',
   templateUrl: './SingleThread.component.html',
   styleUrls: ['./SingleThread.component.css']
 })
+
 export class SingleThreadComponent implements OnInit {
 
-  constructor() { }
+  thread: any;
+
+  replyInfo = {
+    title: '',
+    content: ''
+  };
+
+
+  constructor(
+    private threadService: ThreadsService,
+    public sessionService: SessionService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.threadService.getThread(params.id)
+        .subscribe(thread => this.thread = thread);
+    });
   }
 
+  newReply(id) {
+    this.threadService.newReply(id, this.replyInfo).subscribe(
+      thread => this.thread = thread
+    );
+  }
 }
