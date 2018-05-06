03deb2930fca5446c221b1bb56af8b2b11955eaf
RISTO TAPANI RAIHALA <tyokone@RISTOs-MBP.home>
2018-05-07 00:38:35 +0200

    casi funcionando

---
+++ starter-code/client/src/app/AuthSignup/AuthSignup.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { SessionService } from "../services/Session.service";
+import { Router } from "@angular/router";
+
+
+@Component({
+  selector: 'app-AuthSignup',
+  templateUrl: './AuthSignup.component.html',
+  styleUrls: ['./AuthSignup.component.css']
+})
+export class AuthSignupComponent implements OnInit {
+
+  formInfo = {
+    username: "",
+    email: "",
+    password: ""
+  };
+
+
+  constructor(public sessionService: SessionService, public router: Router) { }
+
+  ngOnInit() {}
+
+  signup(){
+    this.sessionService.signup(this.formInfo).subscribe(() => this.router.navigate(["/home"]));
+  }
+
+}
